83d16e87df8bbbdb3f897b4603dc71bba9c7a496
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-20 19:01:58 -0400

    Feat: Add sunk? method

--- lib/ship.rb
+++ lib/ship.rb
@@ -6,4 +6,9 @@ class Ship
         @health = length
         #Not in love with this solution, let me know if you think there is a better way to store this attribute"
     end
+
+    def sunk?
+        health == 0
+    end
+    
 end

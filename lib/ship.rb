8a3b6df1972b600e88128f0ec7b264f4e3eda17d
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-20 18:55:40 -0400

    Feat: Add health method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,7 +1,9 @@
 class Ship
-    attr_reader :name, :length
+    attr_reader :name, :length, :health
     def initialize(name, length)
         @name = name
         @length = length
+        @health = length
+        #Not in love with this solution, let me know if you think there is a better way to store this attribute"
     end
 end

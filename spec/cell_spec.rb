5485e1015823ada96a52d9f6059e3180570f3901
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-20 20:47:24 -0600

    test: Create test for the empty? method

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,10 +3,40 @@ require './lib/ship'
 require './lib/cell'
 
 RSpec.describe Cell do
-    describe '#initialize' do
-        it "can initialize" do
-            cell = Cell.new("B4")
-            expect(cell).to be_a Cell
-        end
+  describe '#initialize' do
+    it "can initialize" do
+      cell = Cell.new("B4")
+      expect(cell).to be_a Cell
     end
+
+    it 'has readable attributes' do
+      cell = Cell.new("B4")
+
+      expect(cell.coordinate).to eq("B4")
+      expect(cell.ship).to eq(nil)
+    end
+  end
+
+  describe '#empty?' do 
+    it 'can show if the cell is empty' do
+      cell = Cell.new("B4")
+
+      cell.empty?
+
+      expect(cell.empty?).to eq(true)
+    end
+  end
+
+  describe '#place_ship' do 
+    it 'can place a ship in a cell' do
+      cell = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+
+      cell.place_ship(cruiser)
+
+      expect(cell.ship).to eq(cruiser)
+      expect(cell.empty?).to eq(false)
+    end
+      
+  end
 end

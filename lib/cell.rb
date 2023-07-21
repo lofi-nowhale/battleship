77742d0db3c7e218f719dabe837c61db95252b11
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-20 20:54:15 -0600

    feat: Add the #place_ship method

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,10 +2,14 @@ class Cell
   attr_reader :coordinate, :ship
   def initialize(coordinate)
     @coordinate = coordinate
-    @ship = nil
+    @ship
   end
 
   def empty? 
     @ship.nil?
+  end
+
+  def place_ship(new_ship)
+    @ship = new_ship
   end
 end

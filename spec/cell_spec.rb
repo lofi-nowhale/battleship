c4fc07a6666521d1d79f284b168d665d7c4a71d1
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-21 16:18:23 -0600

    test: Create the tests for the render method and its optional argument

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -39,22 +39,61 @@ RSpec.describe Cell do
     end
   end
 
-    describe "fire_upon" do
-        it "can tell if a ship has been fired upon" do
-            cell = Cell.new("B4")
-            cruiser = Ship.new("Cruiser", 3)
+  describe "fire_upon" do
+    it "can tell if a ship has been fired upon" do
+      cell = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
 
-            cell.place_ship(cruiser)
+      cell.place_ship(cruiser)
 
-            expect(cell.fired_upon?).to be false
+      expect(cell.fired_upon?).to be false
 
-            cell.fire_upon
+      cell.fire_upon
 
-            expect(cell.ship.health).to eq(2)
+      expect(cell.ship.health).to eq(2)
 
-            expect(cell.fired_upon?).to be true
-        end
+      expect(cell.fired_upon?).to be true
+    end
+  end
+
+  describe '#render' do 
+    it 'can render the hit status' do 
+      cell_1 = Cell.new("B4")
+      
+      expect(cell_1.render).to eq(".")
+
+      cell_1.fire_upon
+
+      expect(cell_1.render).to eq("M")
+
+      cell_2 = Cell.new("C3")
+      cruiser = Ship.new("Cruiser", 3)
+
+      cell_2.place_ship(cruiser)
+
+      expect(cell_2.render).to eq(".")
     end
 
+    it 'can show a ship with the optional argument' do 
+      cell_1 = Cell.new("B4")
+      cell_2 = Cell.new("C3")
+      cruiser = Ship.new("Cruiser", 3)
+
+      cell_2.place_ship(cruiser)
+      
+      expect(cell_2.render(true)).to eq("S")
+
+      cell_2.fire_upon
+
+      expect(cell_2.render).to eq("H")
+      expect(cruiser.sunk?).to eq(false)
+
+      cruiser.hit
+      cruiser.hit
+
+      expect(cruiser.sunk?).to eq(true)
+      expect(cell_2.render).to eq("X")
+    end
+  end
 
 end

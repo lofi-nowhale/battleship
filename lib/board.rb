308afc7eb71c0c622a500e06dbeb1cb886746187
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-21 19:27:13 -0400

    Feat: Complete the default board

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,20 @@
 class Board
+  attr_reader :cells
   def initialize
+    @cells = Hash.new(0)
+    build_board 
   end
+
+  def build_board
+    ('A'..'D').each do |letter|
+      (1..4).each do |number|
+        coordinate = "#{letter}#{number}"
+        cell_default = '*'
+        @cells[coordinate] = cell_default
+      end
+    end
+  end
+
+
+
 end

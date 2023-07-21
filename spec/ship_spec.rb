1f4b2dd8b37643c5ee7230e39b763026674cfca1
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-20 20:03:34 -0600

    fix: Adjust the spacing for the test file to fit Ruby conventions

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,50 +2,50 @@ require 'rspec'
 require './lib/ship'
 
 RSpec.describe Ship do
-    describe '#initialize' do
-        it "can initialize" do
-            cruiser = Ship.new("Cruiser", 3)
-            expect(cruiser).to be_a Ship
-        end
-
-        it "can have readable attributes" do
-            cruiser = Ship.new("Cruiser", 3)
-            expect(cruiser.name).to eq("Cruiser")
-            expect(cruiser.length).to eq(3)
-        end
+  describe '#initialize' do
+    it "can initialize" do
+      cruiser = Ship.new("Cruiser", 3)
+      expect(cruiser).to be_a Ship
     end
 
-    describe '#health' do
-        it "can read the initial health" do
-            cruiser = Ship.new("Cruiser", 3)
-            expect(cruiser.health).to eq(3)
-        end
+    it "can have readable attributes" do
+      cruiser = Ship.new("Cruiser", 3)
+      expect(cruiser.name).to eq("Cruiser")
+      expect(cruiser.length).to eq(3)
+    end
+  end
 
-        it "can read the health after being hit" do
-            cruiser = Ship.new("Cruiser", 3)
-            expect(cruiser.health).to eq(3)
-            cruiser.hit
-            expect(cruiser.health).to eq(2)
-            cruiser.hit
-            expect(cruiser.health).to eq(1)
-            cruiser.hit
-            expect(cruiser.health).to eq(0)
-        end
-        #We may want to add an edge case when you try to not allow it to go to a negative value"
+  describe '#health' do
+    it "can read the initial health" do
+      cruiser = Ship.new("Cruiser", 3)
+      expect(cruiser.health).to eq(3)
     end
 
-    describe '#sunk?' do
-        it "can tell if the ship is sunk" do
-            cruiser = Ship.new("Cruiser", 3)
-            expect(cruiser.sunk?).to eq(false)
-            cruiser.hit
-            expect(cruiser.sunk?).to eq(false)
-            cruiser.hit
-            expect(cruiser.sunk?).to eq(false)
-            cruiser.hit
-            expect(cruiser.sunk?).to eq(true)
-        end
+    it "can read the health after being hit" do
+      cruiser = Ship.new("Cruiser", 3)
+      expect(cruiser.health).to eq(3)
+      cruiser.hit
+      expect(cruiser.health).to eq(2)
+      cruiser.hit
+      expect(cruiser.health).to eq(1)
+      cruiser.hit
+      expect(cruiser.health).to eq(0)
+    end
     
-    end
+  end
+
+  describe '#sunk?' do
+    it "can tell if the ship is sunk" do
+      cruiser = Ship.new("Cruiser", 3)
+      expect(cruiser.sunk?).to eq(false)
+      cruiser.hit
+      expect(cruiser.sunk?).to eq(false)
+      cruiser.hit
+      expect(cruiser.sunk?).to eq(false)
+      cruiser.hit
+      expect(cruiser.sunk?).to eq(true)
+     end
+    
+  end
 
 end

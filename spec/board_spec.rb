d993c2d3f8f14c7335b7c0b09fe03b9a535716fa
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-23 12:09:29 -0400

    Test: edit tests?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -48,13 +48,13 @@ RSpec.describe Board do
       cruiser = Ship.new("Cruiser", 3)
       submarine = Ship.new("Submarine", 2)
       
-      # expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+      expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
       expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
       expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
       expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
     end
     
-    xit "can tell us if the ship's placement is valid - it will not allow placement to be diagonal" do
+    it "can tell us if the ship's placement is valid - it will not allow placement to be diagonal" do
       board = Board.new
       cruiser = Ship.new("Cruiser", 3)
       submarine = Ship.new("Submarine", 2)
